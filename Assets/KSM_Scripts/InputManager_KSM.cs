567bcb542a367d7f7b246847806feda122ca7700
김승민 <youngrape@naver.com>
2025-09-19 16:33:01 +0900

    인풋 매니저이름 수정

--- Assets/KSM_Scripts/InputManager_KSM.cs
+++ Assets/KSM_Scripts/InputManager_KSM.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class KSM_InputManager_KSM : MonoBehaviour
+public class InputManager_KSM : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
