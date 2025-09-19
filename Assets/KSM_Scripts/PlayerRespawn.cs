1c3f92229c54d589b28a134819ab10ea99da0490
김승민 <youngrape@naver.com>
2025-09-19 15:46:58 +0900

    기존 스크립트 일부 삭제

--- Assets/KSM_Scripts/PlayerRespawn.cs
+++ Assets/KSM_Scripts/PlayerRespawn.cs
@@ -15,8 +15,6 @@ public class PlayerRespawn : MonoBehaviour
 
             Debug.Log("Player has died.");
 
-            FindObjectOfType<PlayerDeathCamera>().SwitchToCaptureCam();
-
             StartCoroutine(RespawnRoutine());
         }
     }
@@ -26,8 +24,6 @@ public class PlayerRespawn : MonoBehaviour
         // 어두워지기
         yield return StartCoroutine(fadeUI.FadeOut());
         
-        FindObjectOfType<PlayerDeathCamera>().SwitchToMainCam();
-
         // 플레이어 위치 이동
         transform.position = checkpoint.position;
 
