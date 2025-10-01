f889bfa4b1427da829347603b2f294752a9d9739
김승민 <youngrape@naver.com>
2025-10-01 14:24:16 +0900

    김대한씨의 요청

--- Assets/KSM_Scripts/InputManager_KSM.cs
+++ Assets/KSM_Scripts/InputManager_KSM.cs
@@ -1,43 +1,17 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using UnityEngine;
 
-public class InputManager_KSM
+public class InputManager_KSM : MonoBehaviour
 {
     public static InputManager_KSM Instance { get; private set; }
-
-    void start()
-    {
-        Init();
-    }
-
-    Managers_KSM s_instance; // 싱글톤 인스턴스
-
-    void Init()
-    {
-        if (s_instance == null) // 전역변수로 선언된 Manager가 있는지
-        {
-            GameObject go = GameObject.Find("@Managers"); // Manager 코드를 담을 객체를 찾는다
-            if (go == null) // 없으면 Manager 스크립트를 붙인 오브젝트를 만든다.
-            {
-                go = new GameObject { name = "@Managers" };
-                go.AddComponent<Managers_KSM>();
-            }
-            DontDestroyOnLoad(go); // 씬 바껴도 안사라짐
-            s_instance = go.GetComponent<Managers_KSM>();
-            // 생성한 게임 오브젝트의 컴포넌트로 붙은 Managers 를 가져온다.
-        }
-    }
 
     [DllImport("user32.dll")]
     private static extern short GetAsyncKeyState(int vKey);
 
-    // 구독자들에게 눌린 키 전달
     public static event Action<List<KeyCode>> OnKeysHeld;
 
-    // 관심 있는 키만 등록
     private KeyCode[] interestedKeys = new KeyCode[]
     {
         KeyCode.W,
@@ -50,21 +24,37 @@ public class InputManager_KSM
         KeyCode.Mouse0,
     };
 
-    private List<KeyCode> _heldKeys = new List<KeyCode>();
+    private List<KeyCode> _pressedKeys = new List<KeyCode>();
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject); // 씬이 바뀌어도 파괴되지 않게 설정
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
 
     void Update()
     {
-        _heldKeys.Clear();
+        _pressedKeys.Clear();
 
         foreach (KeyCode key in interestedKeys)
         {
             short state = GetAsyncKeyState((int)key);
-            if ((state & 0x8000) != 0) // 눌린 상태 확인
+            if ((state & 0x8000) != 0)
             {
-                _heldKeys.Add(key);
+                _pressedKeys.Add(key);
             }
         }
 
-        OnKeysHeld?.Invoke(new List<KeyCode>(_heldKeys));
+        if (_pressedKeys.Count > 0)
+        {
+            OnKeysHeld?.Invoke(new List<KeyCode>(_pressedKeys));
+        }
     }
 }
--- Assets/KSM_Scripts/PlayerMovement_KSM.cs
+++ Assets/KSM_Scripts/PlayerMovement_KSM.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,28 +7,54 @@ using UnityEngine.EventSystems;
 public class PlayerMovement_KSM : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 7f;          // 이동 속도
-    [SerializeField] private float runSpeed = 12f;          // 달리기 조정 변수
+    //[SerializeField] private float runSpeed = 12f;          // 달리기 조정 변수
     [SerializeField] private float rotationSpeed = 10f;     // 회전 속도
-
-    private List<KeyCode> _currentHeldKeys = new List<KeyCode>();
 
     void OnEnable()
     {
-        InputManager_KSM.Instance.OnKeysHeld += HandleKeysHeld;
+        InputManager_KSM.OnKeysHeld += HandleKeysHeld;
     }
 
     void OnDisable()
     {
-        if (InputManager_KSM.Instance != null)
-        {
-            InputManager_KSM.Instance.OnKeysHeld -= HandleKeysHeld;
-        }
+        InputManager_KSM.OnKeysHeld -= HandleKeysHeld;
     }
 
     private void HandleKeysHeld(List<KeyCode> heldKeys)
     {
-        // 최신 키 목록으로 교체
-        _currentHeldKeys = heldKeys;
+        Vector3 moveDirection = Vector3.zero;
+
+        if (heldKeys.Contains(KeyCode.W))
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.forward), 0.2f);
+            transform.position += Vector3.forward * Time.deltaTime * moveSpeed;
+        }
+        if (heldKeys.Contains(KeyCode.A))
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.left), 0.2f);
+            transform.position += Vector3.left * Time.deltaTime * moveSpeed;
+        }
+        if (heldKeys.Contains(KeyCode.S))
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.back), 0.2f);
+            transform.position += Vector3.back * Time.deltaTime * moveSpeed;
+        }
+        if (heldKeys.Contains(KeyCode.D))
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.right), 0.2f);
+            transform.position += Vector3.right * Time.deltaTime * moveSpeed;
+        }
+
+        if (moveDirection != Vector3.zero)
+        {
+            Vector3 normalizedDirection = moveDirection.normalized;
+
+            transform.position += normalizedDirection * moveSpeed * Time.deltaTime;
+
+            Quaternion targetRotation = Quaternion.LookRotation(normalizedDirection);
+
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        }
     }
 
     // 리스폰 컴포넌트
@@ -44,41 +71,41 @@ public class PlayerMovement_KSM : MonoBehaviour
 
     void Update()
     {
-        if (respawn != null && respawn.isDead)
-            return;
+        //if (respawn != null && respawn.isDead)
+        //    return;
 
-        Vector3 moveDirection = Vector3.zero;
+        //Vector3 moveDirection = Vector3.zero;
 
-        if (Input.GetKey(KeyCode.W))
-        {
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.forward), 0.2f);
-            transform.position += Vector3.forward * Time.deltaTime * moveSpeed;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.left), 0.2f);
-            transform.position += Vector3.left * Time.deltaTime * moveSpeed;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.back), 0.2f);
-            transform.position += Vector3.back * Time.deltaTime * moveSpeed;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.right), 0.2f);
-            transform.position += Vector3.right * Time.deltaTime * moveSpeed;
-        }
+        //if (Input.GetKey(KeyCode.W))
+        //{
+        //    transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.forward), 0.2f);
+        //    transform.position += Vector3.forward * Time.deltaTime * moveSpeed;
+        //}
+        //if (Input.GetKey(KeyCode.A))
+        //{
+        //    transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.left), 0.2f);
+        //    transform.position += Vector3.left * Time.deltaTime * moveSpeed;
+        //}
+        //if (Input.GetKey(KeyCode.S))
+        //{
+        //    transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.back), 0.2f);
+        //    transform.position += Vector3.back * Time.deltaTime * moveSpeed;
+        //}
+        //if (Input.GetKey(KeyCode.D))
+        //{
+        //    transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.right), 0.2f);
+        //    transform.position += Vector3.right * Time.deltaTime * moveSpeed;
+        //}
 
-        if (moveDirection != Vector3.zero)
-        {
-            Vector3 normalizedDirection = moveDirection.normalized;
+        //if (moveDirection != Vector3.zero)
+        //{
+        //    Vector3 normalizedDirection = moveDirection.normalized;
 
-            transform.position += normalizedDirection * moveSpeed * Time.deltaTime;
+        //    transform.position += normalizedDirection * moveSpeed * Time.deltaTime;
 
-            Quaternion targetRotation = Quaternion.LookRotation(normalizedDirection);
+        //    Quaternion targetRotation = Quaternion.LookRotation(normalizedDirection);
 
-            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
-        }
+        //    transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        //}
     }
 }
