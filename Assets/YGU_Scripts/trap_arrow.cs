162a361765c235c6d2f60a68cdfdd8c539484143
lentom <meatkiller1212@gmail.com>
2025-12-22 16:15:41 +0900

    Trap 추가
    트랩 인터페이스와 화살 함정, 슬로우 함정 추가. 현재는 데미지 함수, 속도 감소 함수가 제작되지 않음. 플레이어의 스탯을 가져와 함수 제작 필요.

---
+++ Assets/YGU_Scripts/trap_arrow.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trap_arrow : MonoBehaviour, ITrap
+{
+    private float force_0 = 500f;
+
+    [SerializeField]
+    private Rigidbody rb;
+
+    public void ActivateTrap()
+    {
+        Debug.Log("active");
+        ShootArrow();
+    }
+
+    public void DeactivateTrap()
+    {
+        //함정 해제 시 동작 없음
+    }
+
+    void ShootArrow()
+    {
+        Debug.Log("화살 발사!");
+
+
+        // 기존 속도 초기화 (필수!)
+        rb.velocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
+
+        // 화살의 앞 방향(Transform.forward)으로 힘을 줌
+        rb.AddForce(transform.forward * force_0);
+    }
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+}
