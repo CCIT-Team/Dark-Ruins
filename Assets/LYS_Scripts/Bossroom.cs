f0a33f493eccbdd810f7d207df0d6165564ffb82
윤석 이 <nuclearbomb280@gmail.com>
2026-01-09 08:18:03 +0900

    버그제보

--- Assets/LYS_Scripts/Bossroom.cs
+++ Assets/LYS_Scripts/Bossroom.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using LYS_Work;
-using UnityEditor.Animations;
+//using UnityEditor.Animations;
 using UnityEngine;
 
 public class Bossroom : MonoBehaviour
