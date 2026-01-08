e8b5e9f24f874e92ee9c9668b1b0319ed8d54117
daeha <203188790+ntdhk67@users.noreply.github.com>
2026-01-08 12:19:15 +0900

    0108
    반동기반

--- Assets/KDH_Scripts/GunBase.cs
+++ Assets/KDH_Scripts/GunBase.cs
@@ -10,7 +10,7 @@ public abstract class GunBase : ItemBase,IWeapon_KSM
     protected int _loadedBullet=0, _maxBullet=8, _haveBullet=0,v2=0;
     protected float _fireCooltime=0,_reloadCooltime=0;
     protected float _fireCooltimeSet=0.5f,_reloadCooltimeSet=2f;
-    protected bool _zoomOuted = true, _zoomIng = false, e = false;
+    protected bool _zoomOuted = true, _zoomIng = false, e = false,v=false;
     
     protected float _fireLength;
     protected BulletsPool _bulletsPool;
@@ -76,11 +76,13 @@ public abstract class GunBase : ItemBase,IWeapon_KSM
             _zoomOuted = true;
             _camera.fieldOfView = 20;
         }
-        if (v2 > 0)
+        if (v==false&&v2 > 0)
         {
-            Camera.main.transform.forward -= new Vector3(0, 0, 0.1f);
+            Camera.main.transform.localRotation *= Quaternion.Euler(0f, -1f, 0f);
+
             v2--;
         }
+        e = false;
     }
     public override void ItemUse(List<KeyCode> keys)
     {
@@ -93,11 +95,15 @@ public abstract class GunBase : ItemBase,IWeapon_KSM
             _fireCooltime = _fireCooltimeSet;
             Fire();
         }
+        else
+        {
+            v = false;
+        }
 #if UNITY_EDITOR
-        if(_fireCooltime<=0.0f&& _reloadCooltime <= 0.0f&&Input.GetKey(KeyCode.Mouse1))
+        if (_fireCooltime <= 0.0f && _reloadCooltime <= 0.0f && Input.GetKey(KeyCode.Mouse1))
         {
             _fireCooltime = _fireCooltimeSet;
-            _loadedBullet=_maxBullet;
+            _loadedBullet = _maxBullet;
             //Fire();
         }
 #endif
@@ -125,12 +131,14 @@ public abstract class GunBase : ItemBase,IWeapon_KSM
                 Reload(out _);
             }
         }
+
     }
     public virtual void Fire()
     {
 #if UNITY_EDITOR
         Debug.Log("발사");
 #endif
+        v = true;
         v2 ++;
         _loadedBullet--;
         _bulletsPool.Summon(_bullet).GetComponent<FiredBullet>().FireSet(Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, _fireLength)), Camera.main.transform.forward);//위치기준 나중에 하고 활성화나 기타등등 부분 저기서 추가
@@ -138,7 +146,7 @@ public abstract class GunBase : ItemBase,IWeapon_KSM
         _particleSystem.Simulate(0f, true, true);
         _particleSystem.Play();
         //Managers_YGU.Sound.Play("", Sound.Effect);
-        Camera.main.transform.forward += new Vector3(0,0,0.1f);
+        Camera.main.transform.localRotation *= Quaternion.Euler(0f, 1f, 0f);
         _uir.BulletUISet(true, _loadedBullet, HaveBullet());
     }
     public int HaveBullet()
@@ -243,7 +251,6 @@ public abstract class GunBase : ItemBase,IWeapon_KSM
 
     public override void OnDropped()
     {
-        e = false;
         _uir.BulletUISet(false);
     }
     private void OnDisable()
